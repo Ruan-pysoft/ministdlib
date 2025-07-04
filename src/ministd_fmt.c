2e39916c37dc5f859b815da7ab597edc7405bcfe
Ruan <ruan@pysoft.co.za>
2025-07-04 10:05:09 +0200

    Change printf-like suite from macros to functions

--- src/ministd_fmt.c
+++ src/ministd_fmt.c
@@ -17,6 +17,13 @@ fprints(const char ref s, FILE ref file, err_t ref err_out)
 	return fputs(s, file, err_out);
 }
 isz
+printc(char c, err_t ref err_out)
+{ return fprintc(c, stdout, err_out); }
+isz
+prints(const char ref s, err_t ref err_out)
+{ return fprints(s, stdout, err_out); }
+
+isz
 fprintl(long l, FILE ref file, err_t ref err_out)
 {
 	/* max int is 2^63-1 ~= 10^18 * 8, so no more than 20 digits */
@@ -179,6 +186,68 @@ fprintulx(unsigned long ul, FILE ref file, err_t ref err_out)
 
 	return fputs(buf+16-len, file, err_out);
 }
+isz
+printl(long l, err_t ref err_out)
+{ return fprintl(l, stdout, err_out); }
+isz
+printul(unsigned long ul, err_t ref err_out)
+{ return fprintul(ul, stdout, err_out); }
+isz
+printlb(long l, err_t ref err_out)
+{ return fprintlb(l, stdout, err_out); }
+isz
+printulb(unsigned long ul, err_t ref err_out)
+{ return fprintulb(ul, stdout, err_out); }
+isz
+printlx(long l, err_t ref err_out)
+{ return fprintlx(l, stdout, err_out); }
+isz
+printulx(unsigned long ul, err_t ref err_out)
+{ return fprintulx(ul, stdout, err_out); }
+
+#define FPRINT_RAW_S_IMPL(type, suffix) \
+	isz fprint##suffix(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintl((long)suffix, file, err_out); } \
+	isz fprint##suffix##b(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintlb((long)suffix, file, err_out); } \
+	isz fprint##suffix##x(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintlx((long)suffix, file, err_out); } \
+	isz print##suffix(type suffix, err_t ref err_out) \
+	{ return printl((long)suffix, err_out); } \
+	isz print##suffix##b(type suffix, err_t ref err_out) \
+	{ return printl((long)suffix, err_out); } \
+	isz print##suffix##x(type suffix, err_t ref err_out) \
+	{ return printl((long)suffix, err_out); }
+#define FPRINT_RAW_U_IMPL(type, suffix) \
+	isz fprint##suffix(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintul((unsigned long)suffix, file, err_out); } \
+	isz fprint##suffix##b(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintulb((unsigned long)suffix, file, err_out); } \
+	isz fprint##suffix##x(type suffix, FILE ref file, err_t ref err_out) \
+	{ return fprintulx((unsigned long)suffix, file, err_out); } \
+	isz print##suffix(type suffix, err_t ref err_out) \
+	{ return printul((unsigned long)suffix, err_out); } \
+	isz print##suffix##b(type suffix, err_t ref err_out) \
+	{ return printul((unsigned long)suffix, err_out); } \
+	isz print##suffix##x(type suffix, err_t ref err_out) \
+	{ return printul((unsigned long)suffix, err_out); }
+#define FPRINT_IMPL(type, suffix) \
+	FPRINT_RAW_S_IMPL(type, suffix) \
+	FPRINT_RAW_U_IMPL(unsigned type, u##suffix)
+
+FPRINT_RAW_S_IMPL(isz, z)
+FPRINT_RAW_U_IMPL(usz, uz)
+
+FPRINT_IMPL(int, i)
+
+FPRINT_IMPL(short, h)
+
+isz
+fprintp(const ptr p, FILE ref file, err_t ref err_out)
+{ return fprintuzx((usz)p, file, err_out); }
+isz
+printp(const ptr p, err_t ref err_out)
+{ return printuzx((usz)p, err_out); }
 
 #include "ministd_memory.h"
 
