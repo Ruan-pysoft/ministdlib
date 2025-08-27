712600d93d1aba59a4bb356211c6f9d58c9d6506
Ruan <ruan@pysoft.co.za>
2025-08-27 12:54:11 +0200

    Update _syscall functions to not use mov's
    Now for some reason release mode breaks `examples/clone.c`, but not
    debug mode, so I need to investigate that...
    
    References: https://todo.sr.ht/~ruan_p/ministdlib/21

--- include/ministd_syscall.h
+++ include/ministd_syscall.h
@@ -15,53 +15,46 @@
 	)
 #define _syscall1(n, out, arg1) \
 	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
 		"syscall;" \
 		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1) \
+		: "0" ((usz)n), [a1] "D" (arg1) \
 		: "rcx", "r11", "rdi" /* clobber */ \
 	)
 #define _syscall2(n, out, arg1, arg2) \
 	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
-		"mov %[a2], %%rsi;" \
 		"syscall;" \
 		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1), [a2] "g" (arg2) \
-		: "rcx", "r11", "rdi", "rsi" /* clobber */ \
+		: "0" ((usz)n), [a1] "D" (arg1), [a2] "S" (arg2) \
+		: "rcx", "r11" /* clobber */ \
 	)
 #define _syscall3(n, out, arg1, arg2, arg3) \
 	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
-		"mov %[a2], %%rsi;" \
 		"syscall;" \
 		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1), [a2] "g" (arg2), "d" (arg3) \
-		: "rcx", "r11", "rdi", "rsi" /* clobber */ \
+		: "0" ((usz)n), [a1] "D" (arg1), [a2] "S" (arg2), "d" (arg3) \
+		: "rcx", "r11" /* clobber */ \
 	)
-#define _syscall4(n, out, arg1, arg2, arg3, arg4) \
-	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
-		"mov %[a2], %%rsi;" \
-		"mov %[a4], %%r10;" \
-		"syscall;" \
-		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1), [a2] "g" (arg2), "d" (arg3), \
-		  [a4] "g" (arg4) \
-		: "rcx", "r11", "rdi", "rsi", "r10" /* clobber */ \
-	)
-#define _syscall5(n, out, arg1, arg2, arg3, arg4, arg5) \
-	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
-		"mov %[a2], %%rsi;" \
-		"mov %[a4], %%r10;" \
-		"mov %[a5], %%r8;" \
-		"syscall;" \
-		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1), [a2] "g" (arg2), "d" (arg3), \
-		  [a4] "g" (arg4), [a5] "g" (arg5) \
-		: "rcx", "r11", "rdi", "rsi", "r10", "r8" /* clobber */ \
-	)
+#define _syscall4(n, out, arg1, arg2, arg3, arg4) do { \
+		register usz a4 __asm__("r10") = arg4; \
+		__asm__ volatile( \
+			"syscall;" \
+			: "=a" (out) \
+			: "0" ((usz)n), [a1] "D" (arg1), [a2] "S" (arg2), "d" (arg3), \
+			  [a4] "r" (a4) \
+			: "rcx", "r11" /* clobber */ \
+		); \
+	} while (0)
+#define _syscall5(n, out, arg1, arg2, arg3, arg4, arg5) do { \
+		register usz a4 __asm__("r10") = arg4; \
+		register usz a5 __asm__("r8") = arg5; \
+		__asm__ volatile( \
+			"syscall;" \
+			: "=a" (out) \
+			: "0" ((usz)n), [a1] "D" (arg1), [a2] "S" (arg2), "d" (arg3), \
+			  [a4] "r" (a4), [a5] "r" (a5) \
+			: "rcx", "r11" /* clobber */ \
+		); \
+	} while (0)
 #define _syscall6(n, out, arg1, arg2, arg3, arg4, arg5, arg6) do { \
 		register usz a4 __asm__("r10") = arg4; \
 		register usz a5 __asm__("r8") = arg5; \
