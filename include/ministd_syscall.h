ceb30256d7e347f371f0ddd71ed3478a1ff6a53f
Ruan <ruan@pysoft.co.za>
2025-08-27 12:41:54 +0200

    Fix assembly errors in release build with _syscall6
    Instead of `mov`ing variables into the appropriate registers, instead I
    just tell gcc to put them there.
    
    TODO: do the same for the rest of the `_syscall` macros, and see about
    migrating them over from macros to static, always-inline functions, so
    that I can have surety about types (eg, if I pass a negative `isz` value
    for args four, five, or six to the `_syscall6` function, I'm pretty sure
    I get UB, so instead make it clear to the user that the function takes a
    `usz` and let them deal with type conversion)
    
    Fixes: https://todo.sr.ht/~ruan_p/ministdlib/21

--- include/ministd_syscall.h
+++ include/ministd_syscall.h
@@ -55,7 +55,6 @@
 		"mov %[a1], %%rdi;" \
 		"mov %[a2], %%rsi;" \
 		"mov %[a4], %%r10;" \
-		"mov %[a4], %%r10;" \
 		"mov %[a5], %%r8;" \
 		"syscall;" \
 		: "=a" (out) \
@@ -63,19 +62,17 @@
 		  [a4] "g" (arg4), [a5] "g" (arg5) \
 		: "rcx", "r11", "rdi", "rsi", "r10", "r8" /* clobber */ \
 	)
-#define _syscall6(n, out, arg1, arg2, arg3, arg4, arg5, arg6) \
-	__asm__ volatile( \
-		"mov %[a1], %%rdi;" \
-		"mov %[a2], %%rsi;" \
-		"mov %[a4], %%r10;" \
-		"mov %[a4], %%r10;" \
-		"mov %[a5], %%r8;" \
-		"mov %[a6], %%r9;" \
-		"syscall;" \
-		: "=a" (out) \
-		: "0" ((usz)n), [a1] "g" (arg1), [a2] "g" (arg2), "d" (arg3), \
-		  [a4] "g" (arg4), [a5] "g" (arg5), [a6] "g" (arg6) \
-		: "rcx", "r11", "rdi", "rsi", "r10", "r8", "r9" /* clobber */ \
-	)
+#define _syscall6(n, out, arg1, arg2, arg3, arg4, arg5, arg6) do { \
+		register usz a4 __asm__("r10") = arg4; \
+		register usz a5 __asm__("r8") = arg5; \
+		register usz a6 __asm__("r9") = arg6; \
+		__asm__ volatile( \
+			"syscall;" \
+			: "=a" (out) \
+			: "0" ((usz)n), [a1] "D" (arg1), [a2] "S" (arg2), "d" (arg3), \
+			  [a4] "r" (a4), [a5] "r" (a5), [a6] "r" (a6) \
+			: "rcx", "r11" /* clobber */ \
+		); \
+	} while (0)
 
 #endif /* _RM_STD_SYSCALL_H */
