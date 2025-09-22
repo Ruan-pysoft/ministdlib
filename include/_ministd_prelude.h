48f1d7bf8e03402f5deae6b7d9c26dbfa34829d3
Ruan <ruan@pysoft.co.za>
2025-09-22 15:47:54 +0200

    Add comments to <_ministd_prelude.h>

--- include/_ministd_prelude.h
+++ include/_ministd_prelude.h
@@ -1,6 +1,17 @@
 #ifndef _RM_STD_PRELUDE_H
 #define _RM_STD_PRELUDE_H
 
+/* this header is included by every other <ministd*.h> header,
+ * and the purpose of it is to provide a basic interface
+ * which is available everywhere in the library,
+ * and everywhere the library is used.
+ *
+ * furthermore, as far as possible headers should avoid
+ * including any headers other than this one,
+ * to prevent functionality "leaking"
+ * (ie. getting <ministd_io.h> functions when including <ministd_fmt.h>
+ */
+
 #include <ministd_types.h>
 
 #include <ministd.h>
